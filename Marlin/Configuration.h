be67222c8ffc5d3d980f9a74f7765af02aba0a16
longle2710 <p.long351@gmail.com>
2026-01-10 16:44:34 +0700

    modify offset and probe setting

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1511,7 +1511,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -29, -49.5, -2.1 }
+#define NOZZLE_TO_PROBE_OFFSET { -29, -49.5, -2.0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1590,7 +1590,7 @@
  */
 #define Z_CLEARANCE_DEPLOY_PROBE   5 // (mm) Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES  3 // (mm) Z Clearance between probe points
-#define Z_CLEARANCE_MULTI_PROBE     2.5 // (mm) Z Clearance between multiple probes
+#define Z_CLEARANCE_MULTI_PROBE     1.5 // (mm) Z Clearance between multiple probes
 //#define Z_AFTER_PROBING           5 // (mm) Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -1 // (mm) Farthest distance below the trigger-point to go before stopping
